a6aa4cf3f503b9ee43e524fd8c21390a78b7e6ea
Helena Rodriguez Cagide <helenarodrica2@uoc.edu>
2024-04-14 12:00:07 +0200

    teminado ejercicio

---
+++ dist/index.816e7b21.js
@@ -0,0 +1,7 @@
+const darkModeButton = document.getElementById("temaOscuro");
+const body = document.body;
+darkModeButton.addEventListener("click", ()=>{
+    body.classList.toggle("modo-oscuro");
+});
+
+//# sourceMappingURL=index.816e7b21.js.map
